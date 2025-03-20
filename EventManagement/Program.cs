02291f8e8269b68f11e8db485aa11f9a674257d7
Darrel <Darrel@DESKTOP-RBGBVQ7>
2025-03-20 16:31:03 +0800

    Just added a while loop when the user did not choose "5"

--- EventManagement/Program.cs
+++ EventManagement/Program.cs
@@ -23,44 +23,47 @@ namespace EventManagementSystem
 
             Console.Write("Choose an Event: ");
             int userEvent = Convert.ToInt16(Console.ReadLine());
-
-            switch (userEvent)
+       
+            while (userEvent != 5)
             {
-                //this is a CASE statement for CREATING an Event
-                case 1:
-                    Console.Write("Enter Event NAME: ");
-                    string eventName = Console.ReadLine();
-                    break;
 
-                //this is a CASE statement for VIEWING events
-                //I plan to use arrayList in this case, however idk how
-                //in the meantime it will be just this
-                case 2:
-                    Console.Write("UNFINISHED");
-                    break;
+                switch (userEvent)
+                {
+                    //this is a CASE statement for CREATING an Event
+                    case 1:
+                        Console.Write("Enter Event NAME: ");
+                        string eventName = Console.ReadLine();
+                        break;
 
-                //this is a CASE statement for UPDATING events
-                case 3:
-                    Console.Write("Enter The Event to UPDATE: ");
-                    string updateEvent = Console.ReadLine();
-                    break;
+                    //this is a CASE statement for VIEWING events
+                    //I plan to use arrayList in this case, however idk how
+                    //in the meantime it will be just this
+                    case 2:
+                        Console.Write("UNFINISHED");
+                        break;
 
-                //this is a CASE statement for DELETING events
-                case 4:
-                    Console.Write("Enter the Event to DELETE: ");
-                    string deleteEvent = Console.ReadLine();
-                    break;
+                    //this is a CASE statement for UPDATING events
+                    case 3:
+                        Console.Write("Enter The Event to UPDATE: ");
+                        string updateEvent = Console.ReadLine();
+                        break;
 
-                //this is a CASE statement for EXITING the SYSTEM
-                case 5:
-                    Console.WriteLine("THANK YOU FOR USING OUR SYSTEM!");
-                    break;
+                    //this is a CASE statement for DELETING events
+                    case 4:
+                        Console.Write("Enter the Event to DELETE: ");
+                        string deleteEvent = Console.ReadLine();
+                        break;
 
-                //for error handling
-                default:  
-                    Console.WriteLine("NOT AN ACTION");
-                    break;
+                    //this is a CASE statement for EXITING the SYSTEM
+                    case 5:
+                        Console.WriteLine("THANK YOU FOR USING OUR SYSTEM!");
+                        break;
 
+                    //for error handling
+                    default:
+                        Console.WriteLine("NOT AN ACTION");
+                        break;
+                }
             }
         }
     }
